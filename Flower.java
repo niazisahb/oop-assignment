229b4af1cd0e4e2843ac60c3f1a81291d6ebff25
niazisahb <106836188+niazisahb@users.noreply.github.com>
2022-06-03 21:07:04 +0200

    Add files via upload

---
+++ Flower.java
@@ -0,0 +1,22 @@
+public class flower
+{
+  public static void main(String args[])
+   {
+      System.out.println("\n");
+      System.out.println("                 -");
+      System.out.println("               .....");
+      System.out.println("              .......");
+      System.out.println("             -..........-");
+      System.out.println("              .......");
+      System.out.println("               .....");
+      System.out.println("                 _ _");
+      System.out.println("                  |");
+      System.out.println("                  .");
+      System.out.println("                   \\      __");
+      System.out.println("                    \\  _/\\_.\\");
+      System.out.println("                     ./  \\_/__/ ");
+      System.out.println("                     |");
+      System.out.println("                     |");
+      System.out.println("               ...-------...");
+    }
+}
