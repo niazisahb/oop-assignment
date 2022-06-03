ef716ec9dfef5a2059312a4c148bd0cccfcbebd7
niazisahb <106836188+niazisahb@users.noreply.github.com>
2022-06-03 20:53:09 +0200

    Add files via upload

---
+++ ArrayLenth.java
@@ -0,0 +1,8 @@
+public class ArrayLength
+{
+  public static void main(String args[])
+   {
+     String names[] = {"Taha", "Faizan", "Ahmad", "Hassan"};
+      System.out.println(names.length);
+   }
+}
