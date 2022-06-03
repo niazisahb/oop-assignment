a3db63503d6f02ef86b0e87cd5b457e54e0b4232
niazisahb <106836188+niazisahb@users.noreply.github.com>
2022-06-03 20:57:42 +0200

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,12 @@
+ public class Assignment1
+{
+  public static void main(String [] args)
+    {
+      int myInt=24;
+      float myFloat=2.5F;
+      long myLong=580_000L;
+      double myDouble=7.999d;
+      char myChar='C';
+ System.out.print("My Int= "+myInt+"\nMy Float= "+myFloat+"\nMy Long= "+myLong+"\nMy Double= "+myDouble+"\nMy Char= "+myChar);
+    }
+}
